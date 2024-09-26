75d6154a1031fce4cf8627b8d7253e643acdebc4
herakwon <165882056+herakwon@users.noreply.github.com>
2024-09-26 17:57:11 +0900

    Update moderator_chain.py

--- moderator_chain.py
+++ moderator_chain.py
@@ -4,13 +4,19 @@ from langchain_core.output_parsers import StrOutputParser
 from debate_prompts import create_moderator_prompt
 
 def create_moderator_chain(llm: ChatOpenAI):
-    # TODO: create_moderator_prompt()를 사용하여 프롬프트 템플릿 가져오기
-    # TODO: 프롬프트 템플릿, llm, StrOutputParser를 사용하여 체인 구성
-    # TODO: 구성된 체인 반환
-    pass
+    # create_moderator_prompt()를 사용하여 프롬프트 템플릿 가져오기
+    prompt_template = create_moderator_prompt()
 
+    # 프롬프트 템플릿, LLM, StrOutputParser를 사용하여 체인 구성
+    moderator_chain = prompt_template | llm | StrOutputParser()
+    
+    # 구성된 체인을 반환
+    return moderator_chain
+
+    pass
+    
 # 사용 예:
 # llm = ChatOpenAI()
 # moderator_chain = create_moderator_chain(llm)
 # result = moderator_chain.invoke({"topic": "AI ethics", "debate_history": []})
-# print(result)
+# print(result))
