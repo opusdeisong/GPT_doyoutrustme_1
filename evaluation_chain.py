d9ae91b3e692039615221e1478479cab1920437e
starlike <jrinonamu@naver.com>
2024-09-26 17:36:15 +0900

    Refactor evaluation chain creation

--- evaluation_chain.py
+++ evaluation_chain.py
@@ -5,9 +5,13 @@ from debate_prompts import create_evaluation_prompt
 
 def create_evaluation_chain(llm: ChatOpenAI):
     # TODO: create_evaluation_prompt()를 사용하여 프롬프트 템플릿 가져오기
+    evaluation_prompt = create_evaluation_prompt()
+
     # TODO: 프롬프트 템플릿, llm, StrOutputParser를 사용하여 체인 구성
+    evaluation_chain = evaluation_prompt | llm | StrOutputParser()
+
     # TODO: 구성된 체인 반환
-    pass
+    return evaluation_chain
 
 # 사용 예:
 # llm = ChatOpenAI()
