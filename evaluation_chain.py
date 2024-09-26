faadbeeb9372cf93242037a3ea0f352f27d481dd
starlike <jrinonamu@naver.com>
2024-09-26 17:53:19 +0900

    Refactor evaluation and moderator chain creation

--- evaluation_chain.py
+++ evaluation_chain.py
@@ -3,6 +3,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from debate_prompts import create_evaluation_prompt
 
+
 def create_evaluation_chain(llm: ChatOpenAI):
     # TODO: create_evaluation_prompt()를 사용하여 프롬프트 템플릿 가져오기
     evaluation_prompt = create_evaluation_prompt()
@@ -12,6 +13,7 @@ def create_evaluation_chain(llm: ChatOpenAI):
 
     # TODO: 구성된 체인 반환
     return evaluation_chain
+
 
 # 사용 예:
 # llm = ChatOpenAI()
--- moderator_chain.py
+++ moderator_chain.py
@@ -3,11 +3,17 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from debate_prompts import create_moderator_prompt
 
+
 def create_moderator_chain(llm: ChatOpenAI):
     # TODO: create_moderator_prompt()를 사용하여 프롬프트 템플릿 가져오기
+    moderator_prompt = create_moderator_prompt()
+
     # TODO: 프롬프트 템플릿, llm, StrOutputParser를 사용하여 체인 구성
+    moderator_chain = moderator_prompt | llm | StrOutputParser()
+
     # TODO: 구성된 체인 반환
-    pass
+    return moderator_chain
+
 
 # 사용 예:
 # llm = ChatOpenAI()
